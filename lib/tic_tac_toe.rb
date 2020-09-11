dd06c36567a1fda8765e6d95d8fa55953788be00
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-11 22:44:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,10 +35,7 @@ class TicTacToe
     def turn_count
       @board.count{|token| token == "X" || token == "O"}
     end
-    def current_player(board)
-       turn_count(board) % 2 == 0 ? "X" : "O"
-    end
-end
+   
 
 
 
