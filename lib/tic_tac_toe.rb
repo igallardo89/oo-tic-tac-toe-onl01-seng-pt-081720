88e26002e361912da3bda85726679426b4ad00a4
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-11 23:34:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,12 +77,15 @@ class TicTacToe
   end
   
   def play 
-    @board = Array.new(9," ")
-    turn until over?
-    won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
-    puts "Would you like to play again? (Y or N)"
-    gets.strip.downcase == "y" || gets.strip.downcase == "yes" ? play : puts("Goodbye!")
+  while !over?
+    turn
   end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
 end    
 end
 
