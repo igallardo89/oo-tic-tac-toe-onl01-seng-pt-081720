05fd2e2115faef37abc2b100a369c8b29d47a8f7
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-11 22:56:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,7 +53,7 @@ class TicTacToe
       @board[combo[0]] == @board[combo[1]] &&
       @board[combo[1]] == @board[combo[2]] &&
       position_taken?(@board, combo[0])
-      return combo
+      
    end
  
  end
